2aa4e69121597eeab77399f70a1a38cb26fb05d0
Richard Whitney <Richard@Richards-MacBook-Pro.local>
2014-09-18 15:29:04 +0100

    Initial Commit

---
+++ WorkoutBuddy/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  WorkoutBuddy
+//
+//  Created by Richard Whitney on 18/09/2014.
+//  Copyright (c) 2014 Richard Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorkoutBuddy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WorkoutBuddy
+//
+//  Created by Richard Whitney on 18/09/2014.
+//  Copyright (c) 2014 Richard Whitney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
